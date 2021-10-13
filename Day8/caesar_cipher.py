6e68331e247a5a038e062a7ccfb17fe6754750d2
Kyle Kauma <kylekauma@gmail.com>
2021-10-12 22:47:59 -0500

    added instructions, algorithm implementation to caesar cipher

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -2,6 +2,8 @@
 # Project Day 8 - Caesar Cipher
 ###############################
 
+# Build out the caesar cipher logic, implement algorithm
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
