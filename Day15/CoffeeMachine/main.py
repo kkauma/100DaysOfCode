f86ba70d7b931d406ab8bb4bd5682c144dbb0e03
Kyle Kauma <kylekauma@gmail.com>
2021-10-25 21:02:07 -0500

    re-factor code into functions to be used to make different drinks

--- Day15/CoffeeMachine/main.py
+++ Day15/CoffeeMachine/main.py
@@ -1,38 +1,56 @@
 # Coffee Machine Project
 
-while True:
-    MENU = {
-        "espresso": {
-            "ingredients": {
-                "water": 50,
-                "coffee": 18,
-            },
-            "cost": 1.5,
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
         },
-        "latte": {
-            "ingredients": {
-                "water": 200,
-                "milk": 150,
-                "coffee": 24,
-            },
-            "cost": 2.5,
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
         },
-        "cappuccino": {
-            "ingredients": {
-                "water": 250,
-                "milk": 100,
-                "coffee": 24,
-            },
-            "cost": 3.0,
-        }
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
     }
+}
 
-    resources = {
-        "water": 300,
-        "milk": 200,
-        "coffee": 100,
-        "money": 0.0,
-    }
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0.0,
+}
+
+
+def print_report():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${resources['money']}")
+
+
+def ask_for_coins():
+    print("Please insert coins")
+    quarters = int(input("How many quarters?: ")) * .25
+    dimes = int(input("How many dimes?: ")) * .1
+    nickles = int(input("How many nickles?: ")) * .05
+    pennies = int(input("How many pennies?: ")) * .01
+
+
+while True:
+
     user_input = str(input("What would you like? (espresso/latte/cappuccino): "))
 
     # For maintainers of coffee machine to exit program
@@ -40,23 +58,15 @@ while True:
         break
 
     # TODO: 1. Print report of all coffee machine resources
-    elif user_input == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${resources['money']}")
+    print_report()
 
-    elif user_input == "espresso":
+    if user_input == "espresso":
         if resources["water"] < 50:
             print("Sorry, there isn't enough water to make this drink.")
         elif resources["coffee"] < 18:
             print("Sorry, there isn't enough coffee to make this drink.")
         else:
-            print("Please insert coins")
-            quarters = int(input("How many quarters?: ")) * .25
-            dimes = int(input("How many dimes?: ")) * .1
-            nickles = int(input("How many nickles?: ")) * .05
-            pennies = int(input("How many pennies?: ")) * .01
+            ask_for_coins()
 
             user_money = quarters + dimes + nickles + pennies
             drink_cost = MENU["espresso"]["cost"]
