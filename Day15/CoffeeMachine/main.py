eaa208718bf5628af4f7dfd972ce4990b09bee38
Kyle Kauma <kylekauma@gmail.com>
2021-10-25 20:50:01 -0500

    reorganize code, move objects into while loop to preserve cash updates

--- Day15/CoffeeMachine/main.py
+++ Day15/CoffeeMachine/main.py
@@ -1,46 +1,45 @@
 # Coffee Machine Project
 
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-    "money": 0.0,
-}
-
-# TODO: 1. Print report of all coffee machine resources
 while True:
+    MENU = {
+        "espresso": {
+            "ingredients": {
+                "water": 50,
+                "coffee": 18,
+            },
+            "cost": 1.5,
+        },
+        "latte": {
+            "ingredients": {
+                "water": 200,
+                "milk": 150,
+                "coffee": 24,
+            },
+            "cost": 2.5,
+        },
+        "cappuccino": {
+            "ingredients": {
+                "water": 250,
+                "milk": 100,
+                "coffee": 24,
+            },
+            "cost": 3.0,
+        }
+    }
+
+    resources = {
+        "water": 300,
+        "milk": 200,
+        "coffee": 100,
+        "money": 0.0,
+    }
     user_input = str(input("What would you like? (espresso/latte/cappuccino): "))
 
     # For maintainers of coffee machine to exit program
     if user_input == "off":
         break
 
+    # TODO: 1. Print report of all coffee machine resources
     elif user_input == "report":
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
