9b18773da2eba0294c5bf3b71413eaf543a81b23
Kyle Kauma <kylekauma@gmail.com>
2021-10-26 22:23:25 -0500

    refactor ingredient sufficient code into a function and add additional logic

--- Day15/CoffeeMachine/main.py
+++ Day15/CoffeeMachine/main.py
@@ -41,6 +41,12 @@ def print_report():
     print(f"Money: ${resources['money']}")
 
 
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print("Sorry there isn't enough water.")
+
+
 def ask_for_coins():
     print("Please insert coins")
     quarters = int(input("How many quarters?: ")) * .25
@@ -52,36 +58,11 @@ def ask_for_coins():
 is_on = True
 
 while is_on:
-
     user_input = str(input("What would you like? (espresso/latte/cappuccino): "))
-
-    # For maintainers of coffee machine to exit program
     if user_input == "off":
         is_on = False
-
-    # TODO: 1. Print report of all coffee machine resources
-    print_report()
-
-    if user_input == "espresso":
-        if resources["water"] < 50:
-            print("Sorry, there isn't enough water to make this drink.")
-        elif resources["coffee"] < 18:
-            print("Sorry, there isn't enough coffee to make this drink.")
-        else:
-            ask_for_coins()
-
-            user_money = quarters + dimes + nickles + pennies
-            drink_cost = MENU["espresso"]["cost"]
-            change = user_money - drink_cost
-
-            if change < 0:
-                print("Sorry that's not enough money. Money refunded.")
-            else:
-                print(f"Here is {change} in change.")
-                print(f"Here is your {user_input} ☕️ Enjoy!")
-
-            current_money = resources["money"]
-            current_money += drink_cost
-            print(current_money)
-
-# TODO: 2. Add code for latte
+    elif user_input == "report":
+        print_report()
+    else:
+        drink = MENU[user_input]
+        is_resource_sufficient(drink["ingredients"])
