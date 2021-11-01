2f30456125c1b98421f435d26ba97cfac5564cb9
Kyle Kauma <kylekauma@gmail.com>
2021-10-31 19:24:39 -0500

    create instances of a class and add attributes

--- Day17/practice/main.py
+++ Day17/practice/main.py
@@ -1,2 +1,12 @@
 # OOP and creating classes in Python
+# Init is called for every instance a class that gets created
 
+class User:
+    def __init__(self, employee_id, username):
+        self.employee_id = employee_id
+        self.username = username
+
+
+user1 = User("001", "Ace")
+
+print(f"The employee's username is {user1.username}, and their id is {user1.employee_id}.")
