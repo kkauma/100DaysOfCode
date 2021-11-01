4289e4a37c7378968a6ca5f64796f0d659fb9e67
Kyle Kauma <kylekauma@gmail.com>
2021-10-31 19:32:09 -0500

    add methods to class

--- Day17/practice/main.py
+++ Day17/practice/main.py
@@ -2,11 +2,26 @@
 # Init is called for every instance a class that gets created
 
 class User:
-    def __init__(self, employee_id, username):
-        self.employee_id = employee_id
+    def __init__(self, user_id, username):
+        self.id = user_id
         self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def say_hi(self):
+        return f"Hello, {self.username}! Welcome to the metaverse."
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user1 = User("001", "Ace")
+user2 = User("002", "Python")
 
-print(f"The employee's username is {user1.username}, and their id is {user1.employee_id}.")
+user1.follow(user2)
+print(user1.say_hi())
+print(user1.followers)
+print(user1.following)
+print(user2.followers)
+print(user2.following)
