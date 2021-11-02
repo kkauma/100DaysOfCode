652ef2654cbcd81575f6e4a03f51e95e155e3b5a
Kyle Kauma <kylekauma@gmail.com>
2021-11-02 10:07:18 -0500

    fix minor bugs

--- Day17/Quiz_Game/quiz_brain.py
+++ Day17/Quiz_Game/quiz_brain.py
@@ -3,7 +3,11 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = q_list
 
+    def still_has_questions(self):
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+
+
