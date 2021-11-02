9f0d3ba7b4c300302313b2f426bf22374a58ad1d
Kyle Kauma <kylekauma@gmail.com>
2021-11-02 10:04:45 -0500

    add quiz model and refactor

--- Day17/Quiz_Game/main.py
+++ Day17/Quiz_Game/main.py
@@ -1,13 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
-
 for q in question_data:
     question_text = q["text"]
     question_answer = q["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
