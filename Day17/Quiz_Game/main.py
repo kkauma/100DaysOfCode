147eb199c46a8965c69c18b590872c2d2ef3e9f2
Kyle Kauma <kylekauma@gmail.com>
2021-11-03 20:33:41 -0500

    run tests and refactor

--- Day17/Quiz_Game/main.py
+++ Day17/Quiz_Game/main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for q in question_data:
-    question_text = q["text"]
-    question_answer = q["answer"]
+    question_text = q["question"]
+    question_answer = q["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
