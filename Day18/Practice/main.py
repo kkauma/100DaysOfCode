41c8b9c4782fdaf60225a7c2496b32a87851b44d
Kyle Kauma <kylekauma@gmail.com>
2021-11-07 22:09:10 -0600

    refactor code with respect to PEP 8 guidelines

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,27 +1,16 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("magenta")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.left(90)
-
-
-
-
-
-
-
-
-
-
-
+tim = Turtle()
+tim.shape("turtle")
+tim.color("magenta")
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
+tim.forward(100)
+tim.left(90)
 
 screen = Screen()
 screen.exitonclick()
