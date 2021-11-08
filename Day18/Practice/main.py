433c089e9c2b5e4db0534291befea29ee3dba95f
Kyle Kauma <kylekauma@gmail.com>
2021-11-07 22:05:01 -0600

    create square using the turtle module

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,4 +1,27 @@
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("magenta")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.left(90)
 
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
