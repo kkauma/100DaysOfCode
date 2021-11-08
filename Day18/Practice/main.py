7fe74bda85c67d7157058a96d59affa58c7285cc
Kyle Kauma <kylekauma@gmail.com>
2021-11-07 22:15:00 -0600

    import the heroes module and generate a hero

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,16 +1,18 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+import heroes
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("magenta")
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
-
-screen = Screen()
-screen.exitonclick()
+print(heroes.gen())
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("magenta")
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+#
+# screen = Screen()
+# screen.exitonclick()
