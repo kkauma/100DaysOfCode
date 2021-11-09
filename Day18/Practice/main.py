3559240fd432f9d000ca9564cfec3fc4ab3afea6
Kyle Kauma <kylekauma@gmail.com>
2021-11-08 21:41:02 -0600

    create random shape project, add colors

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,12 +1,28 @@
 import turtle as t
+import random
 
 tim = t.Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+colors = ["green", "red", "blue", "orange", "wheat", "magenta",
+          "DarkOrchid", "DeepSkyBlue"]
+
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
 # import heroes
 #
