be9247786d1431d83497a5d0d3d19dc0d0c2de1d
Kyle Kauma <kylekauma@gmail.com>
2021-11-08 21:34:02 -0600

    draw a dashed line using the turtle module

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,18 +1,27 @@
-from turtle import Turtle, Screen
-import heroes
+import turtle as t
 
-print(heroes.gen())
-tim = Turtle()
-tim.shape("turtle")
-tim.color("magenta")
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
-tim.forward(100)
-tim.left(90)
+tim = t.Turtle()
 
-screen = Screen()
-screen.exitonclick()
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+# import heroes
+#
+# print(heroes.gen())
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("magenta")
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+# tim.forward(100)
+# tim.left(90)
+#
+# screen = Screen()
+# screen.exitonclick()
