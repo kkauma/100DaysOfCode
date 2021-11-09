0e00c2f0a5cf3a79bf61d20723db5f511434e8bf
Kyle Kauma <kylekauma@gmail.com>
2021-11-08 21:42:03 -0600

    refactor and delete old code

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -17,27 +17,3 @@ def draw_shape(num_sides):
 for shape_side_n in range(3, 11):
     tim.color(random.choice(colors))
     draw_shape(shape_side_n)
-
-# for _ in range(15):
-#     tim.forward(10)
-#     tim.penup()
-#     tim.forward(10)
-#     tim.pendown()
-
-# import heroes
-#
-# print(heroes.gen())
-# tim = Turtle()
-# tim.shape("turtle")
-# tim.color("magenta")
-# tim.forward(100)
-# tim.left(90)
-# tim.forward(100)
-# tim.left(90)
-# tim.forward(100)
-# tim.left(90)
-# tim.forward(100)
-# tim.left(90)
-#
-# screen = Screen()
-# screen.exitonclick()
