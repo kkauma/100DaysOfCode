c5213836974852ae3bc8e9e129dbe5417b8991dd
Kyle Kauma <kylekauma@gmail.com>
2021-11-10 22:58:37 -0600

    add instructions and comments

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -1,12 +1,14 @@
 import turtle as t
 import random
 
+# Create a turtle instance
 tim = t.Turtle()
 
 colors = ["green", "red", "blue", "orange", "wheat", "magenta",
           "DarkOrchid", "DeepSkyBlue"]
 
 
+# Create a draw shape function
 def draw_shape(num_sides):
     angle = 360 / num_sides
     for _ in range(num_sides):
@@ -14,6 +16,7 @@ def draw_shape(num_sides):
         tim.right(angle)
 
 
+# Loop over shape a user defined number of times
 for shape_side_n in range(3, 11):
     tim.color(random.choice(colors))
     draw_shape(shape_side_n)
