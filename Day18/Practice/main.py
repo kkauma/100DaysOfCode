99632e254e31e1b33218739aa4ae696d05dfa3b5
Kyle Kauma <kylekauma@gmail.com>
2021-11-20 15:49:44 -0600

    add random color functionality using rgb color wheel

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -3,16 +3,24 @@ import random
 
 # Create a turtle instance
 tim = t.Turtle()
+t.colormode(255)
 
-colors = ["green", "red", "blue", "orange", "wheat", "magenta",
-          "DarkOrchid", "DeepSkyBlue"]
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 # Draw a Random Walk - Challenge
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
