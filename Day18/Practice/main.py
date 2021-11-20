4a078a59e7ea2e41fbd72a8990b97c929370822f
Kyle Kauma <kylekauma@gmail.com>
2021-11-20 15:37:12 -0600

    add logic for random walk

--- Day18/Practice/main.py
+++ Day18/Practice/main.py
@@ -6,19 +6,24 @@ tim = t.Turtle()
 
 colors = ["green", "red", "blue", "orange", "wheat", "magenta",
           "DarkOrchid", "DeepSkyBlue"]
+directions = [0, 90, 180, 270]
 
+# Draw a Random Walk - Challenge
+for _ in range(200):
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 # Create a draw shape function
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+#
+# # Loop over shape a user defined number of times
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
 
 
-# Loop over shape a user defined number of times
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
-
-# End of code
