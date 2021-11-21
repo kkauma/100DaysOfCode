d0eea554205609f80b60719c466b6c98e9de6a3e
Kyle Kauma <kylekauma@gmail.com>
2021-11-21 14:19:46 -0600

    extract colors using colorgram

--- Day18/Hirst_Painting_Project/main.py
+++ Day18/Hirst_Painting_Project/main.py
@@ -1,9 +1,26 @@
 import turtle as t
 import random
-import colorgram
+# import colorgram
 
 # Hirst Painting Project
-
+# rgb_colors = []
+# colors = colorgram.extract("image.jpg", 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+color_list = [
+ (196, 9, 68), (244, 230, 74), (212, 155, 90), (19, 117, 174), (168, 169, 27),
+ (107, 180, 209), (218, 131, 166), (163, 74, 30), (238, 72, 34), (5, 35, 87),
+ (26, 139, 72), (124, 181, 142), (219, 76, 122), (240, 222, 3), (82, 17, 80),
+ (175, 47, 90), (9, 59, 35), (12, 166, 213), (239, 160, 190), (126, 33, 21),
+ (9, 44, 130), (52, 164, 114), (4, 104, 63), (4, 88, 99), (142, 209, 220),
+ (100, 31, 13)
+]
 
 
 
