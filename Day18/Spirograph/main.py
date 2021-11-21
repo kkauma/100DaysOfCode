b03696b30bf21cd44e1c41b3050fc0a9d4593d99
Kyle Kauma <kylekauma@gmail.com>
2021-11-21 11:45:56 -0600

    create turtle object and set rgb colors

--- Day18/Spirograph/main.py
+++ Day18/Spirograph/main.py
@@ -1,2 +1,18 @@
+import turtle as t
+import random
+
 # Spirograph Program
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
+
+tim.speed("fastest")
 
