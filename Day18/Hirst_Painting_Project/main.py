a0171783d795e36d4e224f7346ac6ae40a04503d
Kyle Kauma <kylekauma@gmail.com>
2021-11-21 14:30:30 -0600

    finish hirst painting project, refactor code

--- Day18/Hirst_Painting_Project/main.py
+++ Day18/Hirst_Painting_Project/main.py
@@ -1,18 +1,12 @@
 import turtle as t
 import random
-# import colorgram
 
-# Hirst Painting Project
-# rgb_colors = []
-# colors = colorgram.extract("image.jpg", 30)
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b)
-#     rgb_colors.append(new_color)
-#
-# print(rgb_colors)
+t.colormode(255)
+tim = t.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
 color_list = [
  (196, 9, 68), (244, 230, 74), (212, 155, 90), (19, 117, 174), (168, 169, 27),
  (107, 180, 209), (218, 131, 166), (163, 74, 30), (238, 72, 34), (5, 35, 87),
@@ -21,6 +15,26 @@ color_list = [
  (9, 44, 130), (52, 164, 114), (4, 104, 63), (4, 88, 99), (142, 209, 220),
  (100, 31, 13)
 ]
+
+tim.setheading(225)
+tim.forward(250)
+tim.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
+
 
 
 
