f4470907255acc238652ac263429301c3ecd4f11
Kyle Kauma <kylekauma@gmail.com>
2021-11-27 14:02:16 -0600

    initialize motivational quotes project with SMTP and datetime modules

--- Day32/Email/main.py
+++ Day32/Email/main.py
@@ -1,26 +1,17 @@
 # Sending email using Python
 import smtplib
 import datetime as dt
-
-my_email = "cometfunds@gmail.com"
-password ="HelloWorld19"
-
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(from_addr=my_email, to_addrs="jupytercapital@yahoo.com",
-                        msg="Subject:Hello, world!\n\nThanks for your investment, "
-                            "welcome to the fund!")
+import random
 
 now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-if year == 2021:
-    print("Invest in ETH!")
-# print(type(year))
-# print(day_of_week)
+weekday = now.weekday()
+if weekday == 5:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
 
-date_of_birth = dt.datetime(year=1995, month=4, day=1)
-print(date_of_birth)
+    print(quote)
+
+
+
 
