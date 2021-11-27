1d1f7bc67cdd2614a4834dc44a21488ba532b0b3
Kyle Kauma <kylekauma@gmail.com>
2021-11-27 13:46:28 -0600

    clean up minor bugs

--- Day32/Email/main.py
+++ Day32/Email/main.py
@@ -4,10 +4,10 @@ import smtplib
 my_email = "cometfunds@gmail.com"
 password ="HelloWorld19"
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="jupytercapital@yahoo.com",
-                    msg="Subject:Hello, world!\n\nThanks for your investment, "
-                        "welcome to the fund!")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email, to_addrs="jupytercapital@yahoo.com",
+                        msg="Subject:Hello, world!\n\nThanks for your investment, "
+                            "welcome to the fund!")
+
