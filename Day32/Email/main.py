248c05e0cf2d870c1b0d27011e931b44f8c773dd
Kyle Kauma <kylekauma@gmail.com>
2021-11-27 13:57:20 -0600

    import datetime module and create a new datetime object

--- Day32/Email/main.py
+++ Day32/Email/main.py
@@ -1,5 +1,6 @@
 # Sending email using Python
 import smtplib
+import datetime as dt
 
 my_email = "cometfunds@gmail.com"
 password ="HelloWorld19"
@@ -10,4 +11,16 @@ with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.sendmail(from_addr=my_email, to_addrs="jupytercapital@yahoo.com",
                         msg="Subject:Hello, world!\n\nThanks for your investment, "
                             "welcome to the fund!")
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+if year == 2021:
+    print("Invest in ETH!")
+# print(type(year))
+# print(day_of_week)
+
+date_of_birth = dt.datetime(year=1995, month=4, day=1)
+print(date_of_birth)
 
