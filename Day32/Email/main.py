00df72feec1416bce7139219a68f0ed547094b68
Kyle Kauma <kylekauma@gmail.com>
2021-11-27 14:06:43 -0600

    refactor and send motivational quote to email

--- Day32/Email/main.py
+++ Day32/Email/main.py
@@ -3,6 +3,9 @@ import smtplib
 import datetime as dt
 import random
 
+MY_EMAIL = "cometfunds@gmail.com"
+MY_PASSWORD = "HelloWorld19"
+
 now = dt.datetime.now()
 weekday = now.weekday()
 if weekday == 5:
@@ -10,7 +13,13 @@ if weekday == 5:
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
 
-    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}")
 
 
 
