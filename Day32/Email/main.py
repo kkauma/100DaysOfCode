fd73f5482af2a6fdfe42ddff7d25eac802a9eea8
Kyle Kauma <kylekauma@gmail.com>
2021-11-27 13:42:37 -0600

    test SMTP by sending email to separate account

--- Day32/Email/main.py
+++ Day32/Email/main.py
@@ -1,2 +1,13 @@
 # Sending email using Python
+import smtplib
 
+my_email = "cometfunds@gmail.com"
+password ="HelloWorld19"
+
+connection = smtplib.SMTP("smtp.gmail.com", 587)
+connection.starttls()
+connection.login(user=my_email, password=password)
+connection.sendmail(from_addr=my_email, to_addrs="jupytercapital@yahoo.com",
+                    msg="Subject:Hello, world!\n\nThanks for your investment, "
+                        "welcome to the fund!")
+connection.close()
