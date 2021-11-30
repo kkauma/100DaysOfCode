a775eb52fb44aabc552a2e2238a0eab2de5cd1a3
Kyle Kauma <kylekauma@gmail.com>
2021-11-29 21:49:42 -0600

    install pandas, iterate over birthdays dictionary

--- Day32/Birthday_Wisher/main.py
+++ Day32/Birthday_Wisher/main.py
@@ -1,8 +1,13 @@
 # Birthday Wisher Project
 
 import smtplib
+import pandas
 from datetime import datetime
 
 today = (datetime.now().month, datetime.now().day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
+
 
 
