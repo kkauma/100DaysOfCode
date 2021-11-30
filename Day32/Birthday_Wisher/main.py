dd82309da40dd9e842361d8e2889f148394ae2f9
Kyle Kauma <kylekauma@gmail.com>
2021-11-29 21:57:57 -0600

    write message for each letter, finish setting up data retrieval

--- Day32/Birthday_Wisher/main.py
+++ Day32/Birthday_Wisher/main.py
@@ -2,6 +2,7 @@
 
 import smtplib
 import pandas
+import random
 from datetime import datetime
 
 today = (datetime.now().month, datetime.now().day)
@@ -10,4 +11,8 @@ data = pandas.read_csv("birthdays.csv")
 birthdays_dict = {(data_row.month, data_row.day): data_row for (index, data_row) in data.iterrows()}
 
 if today in birthdays_dict:
-    
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_person["name"])
