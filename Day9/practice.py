4c7933d1d1714d1d428bec1e3ff606ae6e4e636b
kkauma <kylekauma@gmail.com>
2023-09-06 20:20:40 -0600

    add a new key to the dictionary and print the new results

--- Day9/practice.py
+++ Day9/practice.py
@@ -1,6 +1,14 @@
 # Dictionaries (key -> value)
 
-cs_concepts = {
+programming_dictionary = {
     "Bug": "An error in a program that presents the program from running as expected.",
-    "Function": "A piece of code that you can easily call over and over again."
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again."
 }
+
+print(programming_dictionary["Function"])
+
+
+# Adding new items to a dictionary
+programming_dictionary["Class"] = "A blueprint for designing a real life object."
+print(programming_dictionary)
