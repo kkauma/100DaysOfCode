0687bda7fe2c16afffd7cd66a6ac214da457c921
kkauma <kylekauma@gmail.com>
2023-09-06 20:30:41 -0600

    practice looping through a python dictionary

--- Day9/practice.py
+++ Day9/practice.py
@@ -6,16 +6,25 @@ programming_dictionary = {
     "Loop": "The action of doing something over and over again."
 }
 
-print(programming_dictionary["Function"])
+# print(programming_dictionary["Function"])
 
 
 # Adding new items to a dictionary
 programming_dictionary["Class"] = "A blueprint for designing a real life object."
-print(programming_dictionary)
+# print(programming_dictionary)
 
 # Create an empty dictionary
 empty_dict = {}
 
 # Wipe an existing dictionary
-programming_dictionary = {}
-print(programming_dictionary)
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A mistake in a codebase that doesn't allow the code to run or compile properly."
+# print(programming_dictionary)
+
+# Loop through a dictionary
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
