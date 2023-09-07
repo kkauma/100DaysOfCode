a74d7085698dfb3e9551bf8c9a020ba5cd978d98
kkauma <kylekauma@gmail.com>
2023-09-06 20:07:03 -0600

    add practice file for dictionaries

---
+++ Day9/practice.py
@@ -0,0 +1,6 @@
+# Dictionaries (key -> value)
+
+cs_concepts = {
+    "Bug": "An error in a program that presents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
