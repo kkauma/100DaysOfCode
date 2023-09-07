031488b3a4b2c902d889cc0f3ed6911038257c42
kkauma <kylekauma@gmail.com>
2023-09-06 20:25:24 -0600

    clear the existing dictionary

--- Day9/practice.py
+++ Day9/practice.py
@@ -12,3 +12,10 @@ print(programming_dictionary["Function"])
 # Adding new items to a dictionary
 programming_dictionary["Class"] = "A blueprint for designing a real life object."
 print(programming_dictionary)
+
+# Create an empty dictionary
+empty_dict = {}
+
+# Wipe an existing dictionary
+programming_dictionary = {}
+print(programming_dictionary)
