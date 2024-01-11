dbe02c917b9385ecc0b26c77b29e570addba9326
kkauma <kylekauma@gmail.com>
2024-01-10 20:57:36 -0700

    practice debugging print statements

---
+++ Day1/debug.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation")
+print('String concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
