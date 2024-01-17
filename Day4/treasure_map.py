98a20c68cc1c76579642b521a04ce7484b0f90fb
kkauma <kylekauma@gmail.com>
2024-01-16 21:17:48 -0700

    add project setup for treasure map

---
+++ Day4/treasure_map.py
@@ -0,0 +1,10 @@
+line1 = ["⬜️","️⬜️","️⬜️"]
+line2 = ["⬜️","⬜️","️⬜️"]
+line3 = ["⬜️️","⬜️️","⬜️️"]
+map = [line1, line2, line3]
+print("Hiding your treasure! X marks the spot.")
+position = input("Where to you want to put the treasure? ")
+
+
+
+print(f"{line1}\n{line2}\n{line3}")
