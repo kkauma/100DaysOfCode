b16b890cc26df32deac58c24cfeb4d90a99b3e64
kkauma <kylekauma@gmail.com>
2024-04-11 19:21:12 -0700

    make small refactor, create output message

--- Day8/paint_calculator.py
+++ Day8/paint_calculator.py
@@ -1,11 +1,11 @@
 import math
 
+def paint_calc(height, width, cover):
+    num_cans = math.ceil((height * width) / cover)
+    print(f"You'll need {num_cans} cans of paint!")
+
 test_h = int(input("Enter the height: "))
 test_w = int(input("Enter the width: "))
 coverage = 5
 
-def paint_calc(height, width):
-    num_cans = math.ceil((height * width) / coverage)
-    print(num_cans)
-
-paint_calc(height=test_h, width=test_w)
+paint_calc(height=test_h, width=test_w, cover=coverage)
