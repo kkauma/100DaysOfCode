c18ed5fa7183a93ac3beaac10a6eb82e5b6345ca
kkauma <kylekauma@gmail.com>
2024-04-14 13:06:15 -0700

    practice adding a dictionary to a list of dictionaries

--- Day9/dict_in_list.py
+++ Day9/dict_in_list.py
@@ -15,3 +15,19 @@ travel_log = [
     }
 ]
 
+# Write a function that will allow new countries to be
+# added to the travel_log
+def add_new_country(name, times_visited, cities_visited):
+    new_country = {}
+    new_country["country"] = name
+    new_country["visits"] = times_visited
+    new_country["cities"] = cities_visited
+    
+    travel_log.append(new_country)
+
+
+add_new_country(country, visits, list_of_cities)
+print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+print(f"My favorite city was {travel_log[2]['cities'][0]}.")
+
+
