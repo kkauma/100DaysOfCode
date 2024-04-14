258084a89443bff78978da83d3eb5e03a9d6bc47
kkauma <kylekauma@gmail.com>
2024-04-14 12:53:18 -0700

    add travel log and boilerplate code

---
+++ Day9/dict_in_list.py
@@ -0,0 +1,17 @@
+country = input("Enter a country: ")
+visits = int(input("Number of visits: "))
+list_of_cities = eval(input("Enter cities: "))
+
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    }
+]
+
